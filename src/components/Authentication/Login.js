e195c60a4a3529a3333cf8f47967f9d89221fe05
Abhishek Verma <43173187+abhishekverma1996@users.noreply.github.com>
2023-05-16 18:30:39 +0530

    Update Login.js

--- src/components/Authentication/Login.js
+++ src/components/Authentication/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
       };
 
       const { data } = await axios.post(
-        "/api/user/login",
+        "https://chatapp-api-oek4.onrender.com/api/user/login",
         { email, password },
         config
       );
